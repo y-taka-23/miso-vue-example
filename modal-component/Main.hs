b0b265eba56cb3f39dab1bf2be89187644e95abe
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-07-14 14:05:01 +0900

    Add the simple modal action

--- modal-component/Main.hs
+++ modal-component/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import           Miso
+import           Miso.String (MisoString)
 
 main :: IO ()
 main = do
@@ -16,18 +17,66 @@ main = do
     , mountPoint = Nothing
     }
 
-data Model = Model
-    deriving (Eq, Show)
+data Model = Model {
+      showModal :: Bool
+    } deriving (Eq, Show)
 
 initialModel :: Model
-initialModel = Model
+initialModel = Model {
+      showModal = False
+    }
 
 data Action =
       NoOp
+    | ShowModal
+    | HideModal
 
 updateModel :: Action -> Model -> Effect Action Model
-updateModel NoOp model =
-    noEff model
+updateModel NoOp model = noEff model
+updateModel ShowModal model = noEff model { showModal = True }
+updateModel HideModal model = noEff model { showModal = False }
 
 viewModel :: Model -> View Action
-viewModel _ = div_ [] [ text "Hello, Modal!" ]
+viewModel model = div_ [ id_ "app" ] $
+    [ button_
+        [ id_ "show-modal", onClick ShowModal ]
+        [ text "Show Modal" ]
+    ] ++
+   (if showModal model then [ attachShadow modalTemplate modal ] else [])
+
+modalTemplate :: View Action
+modalTemplate = div_ [ class_ "modal-mask" ] [
+      div_ [ class_ "modal-wrapper" ] [
+          div_ [ class_ "modal-container" ] [
+              div_ [ class_ "modal-header" ] [
+                  slot [ name_ "header" ] [ modal ]
+                ]
+            , div_ [ class_ "modal-body" ] [
+                  slot [ name_ "body" ] [ text "default body" ]
+                ]
+            , div_ [ class_ "modal-footer" ] [
+                  slot [ name_ "footer" ] [
+                      text "default footer"
+                    , button_
+                        [ class_ "modal-default-button", onClick HideModal ]
+                        [ text "OK" ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+modal :: View Action
+modal = div_ [] [
+      h3_ [ slot_ "header" ] [ text "custom header" ]
+    ]
+
+-- Todo: not implemented yet: shadowRoot.innerHTML
+attachShadow :: View action -> View action -> View action
+attachShadow shadow host = shadow
+
+slot :: [Attribute action] -> [View action] -> View action
+slot = nodeHtml "slot"
+
+slot_ :: MisoString -> Attribute action
+slot_ = textProp "slot"
